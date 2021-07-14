79cb12db66fbfd16f35f2f24d0d2502dfb251ebb
Ralfersin <82880359+Ralfersin@users.noreply.github.com>
2021-07-14 11:43:41 -0300

    update 12-Exercício.py
    Aspas

--- 12-Exercício.py
+++ 12-Exercício.py
@@ -1,7 +1,7 @@
 # Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58
 
 
-print("Hello World!)
+print("Hello World!")
       
 for c in range(10):
       print(c)
